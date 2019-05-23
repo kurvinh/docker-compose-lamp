0757fe7deb01516d964ea1983e5d08fd6f8ea8e0
kurvinh <46738089+kurvinh@users.noreply.github.com>
2019-05-23 12:28:27 +0200

    updating mysql.sh
    taking into account password changes after changing the root password and flushing privileges

--- config/scripts/mysql.sh
+++ config/scripts/mysql.sh
@@ -3,8 +3,8 @@ service mysql stop
 mysql_install_db
 mysqld_safe --skip-grant-tables &
 mysql -uroot -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('root'); FLUSH PRIVILEGES;"
-mysql -uroot -e "set global innodb_file_format = `BARRACUDA`;"
-mysql -uroot -e "set global innodb_large_prefix = `ON`;"
+mysql -uroot -proot -e "set global innodb_file_format = `BARRACUDA`;"
+mysql -uroot -proot -e "set global innodb_large_prefix = `ON`;"
 service mysql stop
 service mysql start
 service apache2 reload
