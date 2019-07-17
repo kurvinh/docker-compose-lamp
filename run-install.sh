365d3e4b03b01b259d137dd4f393f9936cc256af
kurvinh <khendricks@2u.com>
2019-07-17 18:33:30 +0200

    separated mysql into another docker instance

--- run-install.sh
+++ run-install.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-xcode-select --install
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install openldap libiconv
-brew update
+#xcode-select --install
+#ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#brew install openldap libiconv
+#brew update
 #brew install mariadb
 #brew services start mariadb
-brew install git
-brew doctor
+#brew install git
+#brew doctor
 docker-compose up -d
-docker exec  7.2.1-webserver-moodle bash /var/docker-scripts/mysql.sh #mysql init
 cd www/ && git clone -b MOODLE_35_STABLE https://github.com/moodle/moodle.git base-moodle && cd ../ #use HTTPS for base install
